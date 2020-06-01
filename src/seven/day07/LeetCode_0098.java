ee2e364473d708ce2b13d54347a9b8eac81f9717
Wei Xiang <weixiang_91@163.com>
2020-06-01 17:15:51 +0800

    改类名，二叉树两个节点的最近公共祖先

--- src/seven/day07/LeetCode_0098.java
+++ src/seven/day07/LeetCode_0098.java
@@ -35,7 +35,7 @@ import java.util.Stack;
  * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class leetCode_0098 {
+public class LeetCode_0098 {
 
     public static void main(String[] args){
         Integer[] arr = {5,1,4,null,null,3,6};
